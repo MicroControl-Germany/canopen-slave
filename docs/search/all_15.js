7d02bbb7e31c1fc2cd01ee2f32a1ff1ca16659c2
Uwe Koppe <koppe@microcontrol.net>
2026-04-30 16:49:28 +0200

    Update description for CiA 404 add-on

--- docs/search/all_15.js
+++ docs/search/all_15.js
@@ -8,5 +8,8 @@ var searchData=
   ['todo_20list_5',['Todo List',['../todo.html',1,'']]],
   ['tpdo_20mapping_20example_6',['TPDO mapping example',['../cos_example_pdo_map.html#cos_pdo_mapping_tpdo',1,'']]],
   ['tumsgdata_7',['tuMsgData',['../structCpCanMsg__s.html#a04148546b5eab14ba7a779bff177b794',1,'CpCanMsg_s']]],
-  ['tuvalue_8',['tuValue',['../structCosDicEntry__s.html#a8d6b2770fe7fd882fe01b0db680f7b35',1,'CosDicEntry_s']]]
+  ['tuobjx100_5ffieldvalue_8',['tuObjX100_FieldValue',['../structCos404__AI__ProcessData__s.html#a5d569c963428ae5153c990b2221abaa6',1,'Cos404_AI_ProcessData_s']]],
+  ['tuobjx130_5fprocessvalue_9',['tuObjX130_ProcessValue',['../structCos404__AI__ProcessData__s.html#a07c6f458eb7eee4f3267777f284488da',1,'Cos404_AI_ProcessData_s']]],
+  ['tuobjx140_5fnetprocessvalue_10',['tuObjX140_NetProcessValue',['../structCos404__AI__ProcessData__s.html#a50339758ed22640d168557e8bcc0eee0',1,'Cos404_AI_ProcessData_s']]],
+  ['tuvalue_11',['tuValue',['../structCosDicEntry__s.html#a8d6b2770fe7fd882fe01b0db680f7b35',1,'CosDicEntry_s']]]
 ];
--- docs/search/variables_4.js
+++ docs/search/variables_4.js
@@ -1,5 +1,8 @@
 var searchData=
 [
   ['tumsgdata_0',['tuMsgData',['../structCpCanMsg__s.html#a04148546b5eab14ba7a779bff177b794',1,'CpCanMsg_s']]],
-  ['tuvalue_1',['tuValue',['../structCosDicEntry__s.html#a8d6b2770fe7fd882fe01b0db680f7b35',1,'CosDicEntry_s']]]
+  ['tuobjx100_5ffieldvalue_1',['tuObjX100_FieldValue',['../structCos404__AI__ProcessData__s.html#a5d569c963428ae5153c990b2221abaa6',1,'Cos404_AI_ProcessData_s']]],
+  ['tuobjx130_5fprocessvalue_2',['tuObjX130_ProcessValue',['../structCos404__AI__ProcessData__s.html#a07c6f458eb7eee4f3267777f284488da',1,'Cos404_AI_ProcessData_s']]],
+  ['tuobjx140_5fnetprocessvalue_3',['tuObjX140_NetProcessValue',['../structCos404__AI__ProcessData__s.html#a50339758ed22640d168557e8bcc0eee0',1,'Cos404_AI_ProcessData_s']]],
+  ['tuvalue_4',['tuValue',['../structCosDicEntry__s.html#a8d6b2770fe7fd882fe01b0db680f7b35',1,'CosDicEntry_s']]]
 ];
