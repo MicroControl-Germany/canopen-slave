2e711d54332d096bd95c351f9ca25724f1854a6a
Uwe Koppe <koppe@microcontrol.net>
2026-05-14 17:53:43 +0200

    Add sitemap

--- docs/search/typedefs_0.js
+++ docs/search/typedefs_0.js
@@ -2,11 +2,9 @@ var searchData=
 [
   ['cos404aievent_5ffn_0',['Cos404AiEvent_fn',['../cos404ai_8h.html#a1d425ca79221caf1b856dbc50985faca',1,'cos404ai.h']]],
   ['cos404aihandler_5ffn_1',['Cos404AiHandler_fn',['../cos404ai_8h.html#a0d04a2120b28e8da9ac1334a3417dce6',1,'cos404ai.h']]],
-  ['cos404aiscale_5ffn_2',['Cos404AiScale_fn',['../cos404ai_8h.html#afe08773d68fee029e5d81a835b172b93',1,'cos404ai.h']]],
-  ['cos404dt_5ftu_3',['Cos404DT_tu',['../cos404ai_8h.html#ae67a8b3789ba86c148225f0e97141680',1,'cos404ai.h']]],
-  ['cosbootloaderkey_5fts_4',['CosBootloaderKey_ts',['../cos710_8h.html#a3b0f62232459c96537653020d6fd5488',1,'cos710.h']]],
-  ['cosbootloaderseed_5fts_5',['CosBootloaderSeed_ts',['../cos710_8h.html#aa74b34590822fbad9026ee475779dc40',1,'cos710.h']]],
-  ['cospdocom_5fts_6',['CosPdoCom_ts',['../cos__pdo_8h.html#a600a83baf8272ce3e68dd548c275c85c',1,'cos_pdo.h']]],
-  ['cospdomap_5fts_7',['CosPdoMap_ts',['../cos__pdo_8h.html#a2eef50278733fcfa2af8e9dad55ff4f7',1,'cos_pdo.h']]],
-  ['cospdomaphandler_5ffn_8',['CosPdoMapHandler_fn',['../cos__pdo_8h.html#ae786c852755ebe895b128aea7f31ac4c',1,'cos_pdo.h']]]
+  ['cosbootloaderkey_5fts_2',['CosBootloaderKey_ts',['../cos710_8h.html#a3b0f62232459c96537653020d6fd5488',1,'cos710.h']]],
+  ['cosbootloaderseed_5fts_3',['CosBootloaderSeed_ts',['../cos710_8h.html#aa74b34590822fbad9026ee475779dc40',1,'cos710.h']]],
+  ['cospdocom_5fts_4',['CosPdoCom_ts',['../cos__pdo_8h.html#a600a83baf8272ce3e68dd548c275c85c',1,'cos_pdo.h']]],
+  ['cospdomap_5fts_5',['CosPdoMap_ts',['../cos__pdo_8h.html#a2eef50278733fcfa2af8e9dad55ff4f7',1,'cos_pdo.h']]],
+  ['cospdomaphandler_5ffn_6',['CosPdoMapHandler_fn',['../cos__pdo_8h.html#ae786c852755ebe895b128aea7f31ac4c',1,'cos_pdo.h']]]
 ];
--- docs/search/variables_2.js
+++ docs/search/variables_2.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['pfnhandler_0',['pfnHandler',['../structCosPdoMapping__s.html#af79116cf21a12afe0954900b50942b79',1,'CosPdoMapping_s']]],
-  ['pfnoperatingmodeevent_1',['pfnOperatingModeEvent',['../structCos404__AI__Config__s.html#a39d3629c9aa1fa8c2bac27952c74696c',1,'Cos404_AI_Config_s']]],
-  ['pfnscaleprocess_2',['pfnScaleProcess',['../structCos404__AI__Config__s.html#af077882f7a8354d464cd959a38f075a6',1,'Cos404_AI_Config_s']]],
+  ['pfnfilterchangeevent_0',['pfnFilterChangeEvent',['../structCos404__AI__Config__s.html#a0d1813a7252c09f14e1de00ad8467e2d',1,'Cos404_AI_Config_s']]],
+  ['pfnhandler_1',['pfnHandler',['../structCosPdoMapping__s.html#af79116cf21a12afe0954900b50942b79',1,'CosPdoMapping_s']]],
+  ['pfnoperatingmodeevent_2',['pfnOperatingModeEvent',['../structCos404__AI__Config__s.html#a39d3629c9aa1fa8c2bac27952c74696c',1,'Cos404_AI_Config_s']]],
   ['pfnsensortypehandler_3',['pfnSensorTypeHandler',['../structCos404__AI__Config__s.html#a853f825bc030e52a7e47b399ef68b368',1,'Cos404_AI_Config_s']]],
   ['ptscanmsg_4',['ptsCanMsg',['../structCpFifo__s.html#aa53d68f824eb03646858e149ac570234',1,'CpFifo_s']]],
   ['pvddata_5',['pvdData',['../structMcFifoCtrl__s.html#aea1f7f8445f2e90dfad251a57fa811a4',1,'McFifoCtrl_s::pvdData'],['../structCosPdoMapping__s.html#a9c64a08503f9981cefa3da2f20aa4c34',1,'CosPdoMapping_s::pvdData'],['../structCoObject__s.html#a9f9528c0f2b3517626da7d075de76f49',1,'CoObject_s::pvdData']]]
